731725b08f54b172c2998e9de4fd9cbb55baacf3
Matt Gregg <matt.gregg@shopify.com>
2023-01-19 17:13:14 +0000

    prettier

--- polaris-migrator/src/migrations/react-update-page-breadcrumbs/react-update-page-breadcrumbs.ts
+++ polaris-migrator/src/migrations/react-update-page-breadcrumbs/react-update-page-breadcrumbs.ts
@@ -1,40 +1,40 @@
-import type { API, FileInfo } from "jscodeshift";
+import type {API, FileInfo} from 'jscodeshift';
 
-import { POLARIS_MIGRATOR_COMMENT } from "../../constants";
-import { insertCommentBefore, removeJSXAttributes } from "../../utilities/jsx";
+import {POLARIS_MIGRATOR_COMMENT} from '../../constants';
+import {insertCommentBefore, removeJSXAttributes} from '../../utilities/jsx';
 
 export default function reactUpdatePageBreadcrumbs(
   fileInfo: FileInfo,
-  { jscodeshift: j }: API
+  {jscodeshift: j}: API,
 ) {
   const source = j(fileInfo.source);
 
   source
-    .findJSXElements("Page")
+    .findJSXElements('Page')
     .find(j.JSXAttribute, {
       name: {
-        type: "JSXIdentifier",
-        name: "breadcrumbs",
+        type: 'JSXIdentifier',
+        name: 'breadcrumbs',
       },
       value: {
-        type: "JSXExpressionContainer",
+        type: 'JSXExpressionContainer',
       },
     })
     .find(j.JSXExpressionContainer)
     .filter((nodePath) => {
-      if (nodePath.node.expression.type === "Identifier") {
+      if (nodePath.node.expression.type === 'Identifier') {
         insertCommentBefore(
           j,
           j(nodePath).find(j.Identifier).paths()[0],
-          `${POLARIS_MIGRATOR_COMMENT} In this case, you will need to update the breadcrumbs variable to be a single object instead of an array as arrays have been deprecated.`
+          `${POLARIS_MIGRATOR_COMMENT} In this case, you will need to update the breadcrumbs variable to be a single object instead of an array as arrays have been deprecated.`,
         );
       }
 
-      if (nodePath.node.expression.type === "CallExpression") {
+      if (nodePath.node.expression.type === 'CallExpression') {
         insertCommentBefore(
           j,
           j(nodePath).find(j.Identifier).paths()[0],
-          `${POLARIS_MIGRATOR_COMMENT} In this case, you will need to update the breadcrumbs variable to be a single object instead of an array as arrays have been deprecated.`
+          `${POLARIS_MIGRATOR_COMMENT} In this case, you will need to update the breadcrumbs variable to be a single object instead of an array as arrays have been deprecated.`,
         );
         return false;
       }
@@ -49,14 +49,14 @@ export default function reactUpdatePageBreadcrumbs(
         removeJSXAttributes(
           j,
           nodePath.parentPath.parentPath.parentPath.parentPath,
-          "breadcrumbs"
+          'breadcrumbs',
         );
         return;
       }
 
       if (
         arrayOfBreadcrumbs[arrayOfBreadcrumbs.length - 1]?.type !==
-        "ObjectExpression"
+        'ObjectExpression'
       )
         return;
 
